2845fb7e19822455c0e53edf1257f389467928f4
ElHabib <cherq.habib@gmail.com>
2018-08-20 16:01:19 +0100

    Reorganisation du projet

--- src/app/go/ajouter-go/ajouter-go.component.ts
+++ src/app/go/ajouter-go/ajouter-go.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {GoService} from '../../services/go.service';
+import {GoService} from '../../go.service';
 import {Go} from '../../entities/Go';
 import {Router} from '@angular/router';
 
@@ -10,8 +10,7 @@ import {Router} from '@angular/router';
 })
 export class AjouterGoComponent implements OnInit {
 
-  constructor(private  service:GoService,
-              private router:Router) { }
+  constructor(private  service:GoService, private router:Router) { }
 
   ngOnInit() {
   }
