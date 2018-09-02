f7dd1e329bb1eaccf40005d13e54eeb83e0bb0bc
ElHabib <cherq.habib@gmail.com>
2018-09-02 09:56:35 +0100

    finalisation

--- src/app/services/go.service.ts
+++ src/app/services/go.service.ts
@@ -1,85 +1,66 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpResponse} from '@angular/common/http';
 import {Go} from '../entities/Go';
 import {Observable} from 'rxjs';
 import {OneShot} from '../entities/composition/OneShot';
 import {cw} from '../entities/cw';
+import {AuthenticationService} from "./authentication.service";
 
 
-@Injectable()/*
+@Injectable()
+
+
 export class GoService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient,
+              private authentication:AuthenticationService) { }
 
-  getGo(): Observable<Go[]>{
-    return this.http.get<Go[]>('https://one-shot-app.herokuapp.com/go');
+
+  url = 'https://one-shot-app.herokuapp.com';
+
+
+
+
+  getGo(){
+    return this.http.get(this.url+'/go',{
+      headers: new HttpHeaders({ 'Content-Type': 'application/json',
+        'authorization': this.authentication.token }),
+      observe: 'response' as 'response'
+    });
   }
   getGoByCodeSite(codeSite:string){
-    return this.http.get<Go>('https://one-shot-app.herokuapp.com/go/'+codeSite);
+    return this.http.get<Go>(this.url+'/go/'+codeSite,{headers:{'content-type': 'application/json',
+        'authorization': this.authentication.token}});
   }
 
   getOneShot():Observable<OneShot[]>{
-    return this.http.get<OneShot[]>('https://one-shot-app.herokuapp.com/oneshot')
-}
+    return this.http.get<OneShot[]>(this.url+'/oneshot',{headers:{'content-type': 'application/json',
+        'authorization': this.authentication.token}})
+  }
   saveGo(go:Go){
-    return this.http.post('https://one-shot-app.herokuapp.com/go',go);
+    return this.http.post(this.url+'/go',go,{headers:{'content-type': 'application/json',
+        'authorization': this.authentication.token}});
   }
   updateGo(go:Go){
-    return this.http.put('https://one-shot-app.herokuapp.com/go/'+go.codeSite,go)
+    return this.http.put(this.url+'/go/'+go.codeSite,go,{headers:{'content-type': 'application/json',
+        'authorization': this.authentication.token}})
   }
 
 
 
   getCW(){
-    return this.http.get<cw[]>('https://one-shot-app.herokuapp.com/cw')
+    return this.http.get<cw[]>(this.url+'/cw',{headers:{'content-type': 'application/json',
+        'authorization': this.authentication.token}})
   }
 
   getCwByCodeSite(codeSite:string){
-    return this.http.get<cw>('https://one-shot-app.herokuapp.com/cw/'+codeSite);
+    return this.http.get<cw>(this.url+'/cw/'+codeSite,{headers:{'content-type': 'application/json',
+        'authorization': this.authentication.token}});
   }
 
   updateCw(moncw:cw){
-    return this.http.put('https://one-shot-app.herokuapp.com/cw/'+moncw.codeSite,moncw)
-  }
-}*/
-
-export class GoService {
-
-
-
-  constructor(private http:HttpClient) { }
-
-  getGo(): Observable<Go[]>{
-    return this.http.get<Go[]>('https://one-shot-app.herokuapp.com/go',{headers:{'content-type': 'application/json',
-        'authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNTM2NjgzMDYyLCJyb2xlcyI6W119.I8gQJ-3ckTKl2FxNc74G-CMi_' +
-          '7NV90Za0uBrr1UytdUiDQbw76iLlLhMtukasCqlZ-SXXl-S3Q_Qzm1QVCAmlQ'}});
-  }
-  getGoByCodeSite(codeSite:string){
-    return this.http.get<Go>('http://localhost:8090/go/'+codeSite);
-  }
-
-  getOneShot():Observable<OneShot[]>{
-    return this.http.get<OneShot[]>('http://localhost:8090/oneshot')
-  }
-  saveGo(go:Go){
-    return this.http.post('http://localhost:8090/go',go);
-  }
-  updateGo(go:Go){
-    return this.http.put('http://localhost:8090/go/'+go.codeSite,go)
-  }
-
-
-
-  getCW(){
-    return this.http.get<cw[]>('http://localhost:8090.com/cw')
-  }
-
-  getCwByCodeSite(codeSite:string){
-    return this.http.get<cw>('http://localhost:8090/cw/'+codeSite);
-  }
-
-  updateCw(moncw:cw){
-    return this.http.put('http://localhost:8090/cw/'+moncw.codeSite,moncw)
+    return this.http.put(this.url+'/cw/'+moncw.codeSite,moncw,{headers:{'content-type': 'application/json',
+        'authorization': this.authentication.token}})
   }
 
 }
