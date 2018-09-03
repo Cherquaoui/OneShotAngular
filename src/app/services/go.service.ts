40614742f03e22bf589bcf868865dd7c193d75fd
Cherquaoui <cherq.habib@gmail.com>
2018-09-03 10:52:16 +0100

    add login

--- src/app/services/go.service.ts
+++ src/app/services/go.service.ts
@@ -16,49 +16,44 @@ export class GoService {
               private authentication:AuthenticationService) { }
 
 
-  url = 'http://localhost:8090';
-
-
-
-
   getGo(){
-    return this.http.get<Go[]>(this.url+'/go',{
+    return this.http.get<Go[]>(this.authentication.getUrl()+'/go',{
       headers: new HttpHeaders({ 'Content-Type': 'application/json',
         'authorization': this.authentication.getToken() })
     });
   }
   getGoByCodeSite(codeSite:string){
-    return this.http.get<Go>(this.url+'/go/'+codeSite,{headers:{'content-type': 'application/json',
+    return this.http.get<Go>(this.authentication.getUrl()+'/go/'+codeSite,{headers:{'content-type': 'application/json',
         'authorization': this.authentication.getToken()}});
   }
 
   getOneShot():Observable<OneShot[]>{
-    return this.http.get<OneShot[]>(this.url+'/oneshot',{headers:{'content-type': 'application/json',
+    return this.http.get<OneShot[]>(this.authentication.getUrl()+'/oneshot',{headers:{'content-type': 'application/json',
         'authorization': this.authentication.getToken()}})
   }
   saveGo(go:Go){
-    return this.http.post(this.url+'/go',go,{headers:{'content-type': 'application/json',
+    return this.http.post(this.authentication.getUrl()+'/go',go,{headers:{'content-type': 'application/json',
         'authorization': this.authentication.getToken()}});
   }
   updateGo(go:Go){
-    return this.http.put(this.url+'/go/'+go.codeSite,go,{headers:{'content-type': 'application/json',
+    return this.http.put(this.authentication.getUrl()+'/go/'+go.codeSite,go,{headers:{'content-type': 'application/json',
         'authorization': this.authentication.getToken()}})
   }
 
 
 
   getCW(){
-    return this.http.get<cw[]>(this.url+'/cw',{headers:{'content-type': 'application/json',
+    return this.http.get<cw[]>(this.authentication.getUrl()+'/cw',{headers:{'content-type': 'application/json',
         'authorization': this.authentication.getToken()}})
   }
 
   getCwByCodeSite(codeSite:string){
-    return this.http.get<cw>(this.url+'/cw/'+codeSite,{headers:{'content-type': 'application/json',
+    return this.http.get<cw>(this.authentication.getUrl()+'/cw/'+codeSite,{headers:{'content-type': 'application/json',
         'authorization': this.authentication.getToken()}});
   }
 
   updateCw(moncw:cw){
-    return this.http.put(this.url+'/cw/'+moncw.codeSite,moncw,{headers:{'content-type': 'application/json',
+    return this.http.put(this.authentication.getUrl()+'/cw/'+moncw.codeSite,moncw,{headers:{'content-type': 'application/json',
         'authorization': this.authentication.getToken()}})
   }
 
