b6d534d25d17bf50f1219767a9d5dbb6f5b04783
Cherquaoui <cherq.habib@gmail.com>
2018-09-05 17:26:04 +0100

    pagination test Heroku

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -5,8 +5,8 @@ import {Router} from '@angular/router';
 @Injectable()
 export class AuthenticationService {
 
-/*  private url = 'https://one-shot-app.herokuapp.com';*/
-  private  url = 'http://localhost:8090';
+  private url = 'https://one-shot-app.herokuapp.com';
+/*  private  url = 'http://localhost:8090';*/
   private token: string = null;
 
   private isLog = false;
